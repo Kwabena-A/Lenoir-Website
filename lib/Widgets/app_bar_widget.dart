b9a27b176e5854eb4248e0b80d8f0bc621f6b3e5
kb <kwabena14817@gmail.com>
2025-11-12 10:39:44 +0400

    App Bar Animation

--- lib/Widgets/app_bar_widget.dart
+++ lib/Widgets/app_bar_widget.dart
@@ -1,34 +1,84 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:lenoir_website/Widgets/home_page_widget.dart';
 import 'package:lenoir_website/Widgets/text_button_widget.dart';
 
-class AppBarWidget extends StatelessWidget {
+class AppBarWidget extends StatefulWidget {
   const AppBarWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: double.infinity,
-      color: Colors.black,
-      alignment: Alignment.centerLeft,
-      padding: EdgeInsets.symmetric(horizontal: 20),
+  State<AppBarWidget> createState() => _AppBarWidgetState();
+}
 
-      child: Row(
-        children: [
-          Padding(
-            padding: EdgeInsets.only(right: 20),
-            child: Transform.scale(
-              scale: 0.7,
-              child: Image.asset("assets/white-icon.png"),
+class _AppBarWidgetState extends State<AppBarWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation _animationOffset;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 200),
+    );
+    _animationOffset = Tween(begin: 0.0, end: -100).animate(_controller);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double deltaScrollPosition = 0;
+    double initialScrollPosition = homePageScrollController.offset;
+    late double finalScrollPosition;
+    Timer.periodic(Duration(milliseconds: 10), (timer) {
+      finalScrollPosition = homePageScrollController.offset;
+      deltaScrollPosition = initialScrollPosition - finalScrollPosition;
+      initialScrollPosition = finalScrollPosition;
+
+      if (deltaScrollPosition < 0) {
+        if (_controller.status != AnimationStatus.forward) {
+          _controller.forward();
+        }
+      }
+      if (deltaScrollPosition > 0) {
+        if (_controller.status != AnimationStatus.reverse) {
+          _controller.reverse();
+        }
+      }
+    });
+
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.translate(
+          offset: Offset(0, _animationOffset.value),
+          child: Container(
+            height: 50,
+            width: double.infinity,
+            color: Colors.black,
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+
+            child: Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: Transform.scale(
+                    scale: 0.7,
+                    child: Image.asset("assets/white-icon.png"),
+                  ),
+                ),
+                TextButtonWidget(text: "Racing"),
+                TextButtonWidget(text: "About Us"),
+                TextButtonWidget(text: "Our Cars"),
+                TextButtonWidget(text: "Schedule"),
+              ],
             ),
           ),
-          TextButtonWidget(text: "Racing"),
-          TextButtonWidget(text: "About Us"),
-          TextButtonWidget(text: "Our Cars"),
-          TextButtonWidget(text: "Schedule"),
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/Widgets/home_page_widget.dart
+++ lib/Widgets/home_page_widget.dart
@@ -2,12 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:lenoir_website/Widgets/image_cycle_widget.dart';
 import 'package:lenoir_website/Widgets/team_info_widget.dart';
 
+final ScrollController homePageScrollController = ScrollController();
+
 class HomePageWidget extends StatelessWidget {
   const HomePageWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
+      controller: homePageScrollController,
       child: Column(children: [ImageCycleWidget(), TeamInfoWidget()]),
     );
   }
