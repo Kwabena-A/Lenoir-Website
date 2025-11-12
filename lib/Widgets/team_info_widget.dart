009f6188f3113a5dc125f7069768ca14ea3b8c27
kb <kwabena14817@gmail.com>
2025-11-12 08:37:09 +0400

    Added animation, info swap still incomplete

--- lib/Widgets/team_info_widget.dart
+++ lib/Widgets/team_info_widget.dart
@@ -37,7 +37,7 @@ class _TeamInfoWidgetState extends State<TeamInfoWidget> {
             ),
           ],
         ),
-        MemberDescription(),
+        MemberDescription(width: MediaQuery.of(context).size.width),
       ],
     );
   }
@@ -64,7 +64,6 @@ class _PersonInfoState extends State<PersonInfo>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation _animation;
-  bool isHovering = false;
 
   @override
   void initState() {
@@ -100,7 +99,6 @@ class _PersonInfoState extends State<PersonInfo>
               }
             },
             onExit: (event) {
-              isHovering = false;
               if (_controller.status != AnimationStatus.reverse) {
                 _controller.reverse(from: _controller.value);
               }
@@ -121,52 +119,129 @@ class _PersonInfoState extends State<PersonInfo>
   }
 }
 
-class MemberDescription extends StatelessWidget {
-  const MemberDescription({super.key});
+class MemberDescription extends StatefulWidget {
+  MemberDescription({super.key, required this.width});
+  double width;
+
+  @override
+  State<MemberDescription> createState() => _MemberDescriptionState();
+}
+
+class _MemberDescriptionState extends State<MemberDescription>
+    with SingleTickerProviderStateMixin {
+  PersonInfo updatePerson() {
+    return PersonInfo(
+      name: currentPerson.value.name,
+      image: currentPerson.value.image,
+      role: currentPerson.value.role,
+      description: currentPerson.value.description,
+    );
+  }
+
+  late PersonInfo _displayPerson = updatePerson();
+
+  late AnimationController _controller;
+  late Animation _animation;
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    _animation = Tween<double>(
+      begin: 0.0,
+      end: widget.width,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOut));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
       valueListenable: currentPerson,
       builder: (context, value, child) {
-        return Container(
-          width: double.infinity,
-          color: Colors.black,
-          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                currentPerson.value.name,
-                style: TextStyle(
-                  fontFamily: "oddlini",
-                  color: Colors.white,
-                  fontSize: 30,
-                  height: 0,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  currentPerson.value.role,
-                  style: GoogleFonts.getFont(
-                    "Montserrat",
-                    color: Colors.white,
-                    fontSize: 15,
+        return Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              color: Colors.black,
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    _displayPerson.name,
+                    style: TextStyle(
+                      fontFamily: "oddlini",
+                      color: Colors.white,
+                      fontSize: 30,
+                      height: 0,
+                    ),
                   ),
-                ),
-              ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      _displayPerson.role,
+                      style: GoogleFonts.getFont(
+                        "Montserrat",
+                        color: Colors.white,
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
 
-              Text(
-                currentPerson.value.description,
-                style: GoogleFonts.getFont(
-                  "Montserrat",
-                  color: Colors.white,
-                  fontSize: 15,
-                ),
+                  Text(
+                    _displayPerson.description,
+                    style: GoogleFonts.getFont(
+                      "Montserrat",
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                if (_displayPerson.name != currentPerson.value.name) {
+                  if (_controller.status != AnimationStatus.forward) {
+                    _controller.forward(from: _controller.value);
+                  }
+                }
+
+                if (_controller.status == AnimationStatus.completed) {
+                  print("Completed");
+                  _displayPerson = updatePerson();
+                  _controller.reverse();
+                }
+
+                return Positioned.fill(
+                  child: ClipRect(
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: SizedBox(
+                        child: SizedBox(
+                          height: double.infinity,
+                          width: _animation.value,
+                          child: Container(color: Color(0xFF323232)),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
         );
       },
     );
