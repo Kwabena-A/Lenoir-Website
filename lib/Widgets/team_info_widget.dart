7da9a52ed42030b1c51195ab86bb32a74922a5cc
kb <kwabena14817@gmail.com>
2025-11-12 09:27:25 +0400

    Created Swipe Animation

--- lib/Widgets/team_info_widget.dart
+++ lib/Widgets/team_info_widget.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 import '../Data/values.dart';
 
+// Logic
 class TeamInfoWidget extends StatefulWidget {
   const TeamInfoWidget({super.key});
 
@@ -13,36 +14,45 @@ class TeamInfoWidget extends StatefulWidget {
 class _TeamInfoWidgetState extends State<TeamInfoWidget> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: teamMembers,
-            ),
-            IgnorePointer(
-              child: Container(
-                height: MediaQuery.of(context).size.height - 100,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0xB9000000), Colors.transparent],
-                    begin: AlignmentGeometry.topCenter,
-                    end: AlignmentGeometry.bottomCenter,
-                    stops: [0, 0.5],
+    return SizedBox(
+      height: MediaQuery.of(context).size.height - 50,
+      child: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: teamMembers,
                   ),
                 ),
-              ),
+                Positioned.fill(
+                  child: IgnorePointer(
+                    child: Container(
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: [Color(0xB9000000), Colors.transparent],
+                          begin: AlignmentGeometry.topCenter,
+                          end: AlignmentGeometry.bottomCenter,
+                          stops: [0, 0.5],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-        MemberDescription(),
-      ],
+          ),
+          MemberDescription(),
+        ],
+      ),
     );
   }
 }
 
+// Person Blocks
 class PersonInfo extends StatefulWidget {
   const PersonInfo({
     super.key,
@@ -105,11 +115,11 @@ class _PersonInfoState extends State<PersonInfo>
                 _controller.reverse(from: _controller.value);
               }
             },
-            child: SizedBox(
-              height: MediaQuery.of(context).size.height - 100,
-              child: ClipRRect(
-                child: Transform.scale(
-                  scale: _animation.value,
+            child: ClipRect(
+              child: Transform.scale(
+                scale: _animation.value,
+                child: SizedBox(
+                  height: double.infinity,
                   child: Image.asset(widget.image, fit: BoxFit.cover),
                 ),
               ),
@@ -121,52 +131,158 @@ class _PersonInfoState extends State<PersonInfo>
   }
 }
 
-class MemberDescription extends StatelessWidget {
+// Person Description
+class MemberDescription extends StatefulWidget {
   const MemberDescription({super.key});
+
+  @override
+  State<MemberDescription> createState() => _MemberDescriptionState();
+}
+
+class _MemberDescriptionState extends State<MemberDescription>
+    with TickerProviderStateMixin {
+  late AnimationController _controllerTitle;
+  late Animation _animationTitleOffset;
+  late Animation _animationTitleOpacity;
+
+  late AnimationController _controllerRole;
+  late Animation _animationRoleOffset;
+  late Animation _animationRoleOpacity;
+
+  late AnimationController _controllerDescription;
+  late Animation _animationDescriptionOffset;
+  late Animation _animationDescriptionOpacity;
+
+  late Listenable _merged;
+
+  void reset_controllers() {
+    _controllerTitle.reset();
+    _controllerRole.reset();
+    _controllerDescription.reset();
+  }
+
+  @override
+  void initState() {
+    // Title
+    _controllerTitle = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _animationTitleOffset = Tween(
+      begin: -10.0,
+      end: 0.0,
+    ).animate(_controllerTitle);
+    _animationTitleOpacity = ColorTween(
+      begin: Colors.transparent,
+      end: Colors.white,
+    ).animate(_controllerTitle);
+
+    // Role
+    _controllerRole = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _animationRoleOffset = Tween(
+      begin: -10.0,
+      end: 0.0,
+    ).animate(_controllerRole);
+    _animationRoleOpacity = ColorTween(
+      begin: Colors.transparent,
+      end: Colors.white,
+    ).animate(_controllerRole);
+
+    // Description
+    _controllerDescription = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _animationDescriptionOffset = Tween(
+      begin: -10.0,
+      end: 0.0,
+    ).animate(_controllerDescription);
+    _animationDescriptionOpacity = ColorTween(
+      begin: Colors.transparent,
+      end: Colors.white,
+    ).animate(_controllerDescription);
+
+    // Chain Logic
+    _controllerTitle.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controllerRole.forward();
+      }
+    });
+    _controllerRole.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controllerDescription.forward();
+      }
+    });
+
+    _merged = Listenable.merge([
+      _controllerTitle,
+      _controllerRole,
+      _controllerDescription,
+    ]);
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder(
       valueListenable: currentPerson,
       builder: (context, value, child) {
-        return Container(
-          width: double.infinity,
-          color: Colors.black,
-          padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                currentPerson.value.name,
-                style: TextStyle(
-                  fontFamily: "oddlini",
-                  color: Colors.white,
-                  fontSize: 30,
-                  height: 0,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 10.0),
-                child: Text(
-                  currentPerson.value.role,
-                  style: GoogleFonts.getFont(
-                    "Montserrat",
-                    color: Colors.white,
-                    fontSize: 15,
+        reset_controllers();
+        _controllerTitle.forward();
+        return AnimatedBuilder(
+          animation: _merged,
+          builder: (context, child) {
+            return Container(
+              width: double.infinity,
+              height: 150,
+              color: Colors.black,
+              padding: EdgeInsets.symmetric(horizontal: 40, vertical: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Transform.translate(
+                    offset: Offset(0, _animationTitleOffset.value),
+                    child: Text(
+                      currentPerson.value.name,
+                      style: TextStyle(
+                        fontFamily: "oddlini",
+                        color: _animationTitleOpacity.value,
+                        fontSize: 30,
+                        height: 0,
+                      ),
+                    ),
                   ),
-                ),
-              ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 10.0),
+                    child: Transform.translate(
+                      offset: Offset(0, _animationRoleOffset.value),
+                      child: Text(
+                        currentPerson.value.role,
+                        style: GoogleFonts.getFont(
+                          "Montserrat",
+                          color: _animationRoleOpacity.value,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
 
-              Text(
-                currentPerson.value.description,
-                style: GoogleFonts.getFont(
-                  "Montserrat",
-                  color: Colors.white,
-                  fontSize: 15,
-                ),
+                  Text(
+                    currentPerson.value.description,
+                    style: GoogleFonts.getFont(
+                      "Montserrat",
+                      color: _animationDescriptionOpacity.value,
+                      fontSize: 15,
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            );
+          },
         );
       },
     );
